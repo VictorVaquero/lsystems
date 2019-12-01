8cc5b6b516d4cb832ffec57f261cf89dbab5e6da
Victor <victorvaquero.etereot@gmail.com>
2019-12-01 23:23:54 +0100

    Update flat function

--- js/auxiliares.js
+++ js/auxiliares.js
@@ -1,10 +1,24 @@
-Object.defineProperty(Array.prototype, 'flat', {
-    value: function(depth = 1) {
-      return this.reduce(function (flat, toFlatten) {
-        return flat.concat((Array.isArray(toFlatten) && (depth>1)) ? toFlatten.flat(depth-1) : toFlatten);
-      }, []);
-    }
-});
+if (!Array.prototype.flat)
+{
+    Object.defineProperty(Array.prototype, 'flat',
+    {
+        value: function(depth = 1, stack = [])
+        {
+            for (let item of this)
+            {
+                if (item instanceof Array && depth > 0)
+                {
+                    item.flat(depth - 1, stack);
+                }
+                else {
+                    stack.push(item);
+                }
+            }
+
+            return stack;
+        }
+    });
+}
 
 
 function add(v1, v2) {
